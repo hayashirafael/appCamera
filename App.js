d7d07d3713b625669fa9668b5a30cd9c67a58e15
Rafael Hayashi <rafaelhayashi8@gmail.com>
2021-05-31 21:53:43 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text, StatusBar } from 'react-native';
+
+export default function projetoCamera() {
+ return (
+   <View>
+     <StatusBar hidden={true}/>
+     <Text>Ola</Text>
+   </View>
+  );
+}
