a4fa2635ef6af66bd090febbf625e45f35393b68
Rafael Hayashi <rafaelhayashi8@gmail.com>
2021-06-01 18:15:14 -0300

    adicionando componente da camera e estilizando botoes

--- App.js
+++ App.js
@@ -1,11 +1,62 @@
-import React from 'react';
-import { View, Text, StatusBar } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StatusBar, StyleSheet, TouchableOpacity } from 'react-native';
+import { RNCamera } from 'react-native-camera'
 
 export default function projetoCamera() {
+  const [tipo, setTipo] = useState(RNCamera.Constants.Type.back) //iniciar com a camera traseira
+
  return (
-   <View>
-     <StatusBar hidden={true}/>
-     <Text>Ola</Text>
+   <View style={styles.container}>
+     <RNCamera
+     style={styles.preview}
+     type={tipo} // setando o modo que a camera ira iniciar
+     flashMode={RNCamera.Constants.FlashMode.off}
+     androidCameraPermissionOptions={{
+       title: 'Permissao para usar a camera',
+       message: 'Por gentileza, autorizar o uso da camera',
+       buttonPositive: 'Ok',
+       buttonNegative: 'Cancelar'
+     }}
+     >
+       {({ camera, status, recordAudioPermissionStatus } ) => {
+         if(status !== 'READY') return <View/>
+         return(
+           <View
+           style={{marginBottom: 35, flexDirection: 'row', alignItems: 'flex-end', justifyContent: 'space-between'}}>
+
+             <TouchableOpacity onPress={() => {}} style={styles.capture} >
+               <Text>Tirar foto</Text>
+             </TouchableOpacity>
+             
+             <TouchableOpacity onPress={() => {}} style={styles.capture} >
+               <Text>Album</Text>
+             </TouchableOpacity>
+
+           </View>
+         )
+       }}
+
+     </RNCamera>
    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    alignSelf: 'center',
+    margin: 20
+  }
+})
